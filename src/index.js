014e97b82dafcd0509ba0092a5a82fea0d892c21
jeff <hi@jeffwolff.net>
2020-12-30 18:16:51 -0800

    add mobile menu and gradient animation on testimonial block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css';
 import './style.scss';
 
+
 const next = document.querySelector('.testimonial-block__forward-arrow');
 const prev = document.querySelector('.testimonial-block__back-arrow');
 
@@ -21,9 +22,11 @@ class Carousel {
 
     next.addEventListener('click', () => this.step(1))
     prev.addEventListener('click', () => this.step(-1))
-    carouselWrap.addEventListener('mouseover',() => this.interrupt = true)
-    carouselWrap.addEventListener('mouseout',() => this.interrupt = false)
-    carouselWrap.addEventListener('mouseout',() => this.interrupt = false)
+
+    carouselBlock.addEventListener('mouseover',() => this.interrupt = true)
+    carouselBlock.addEventListener('mouseout',() => this.interrupt = false)
+    carouselBlock.addEventListener('mouseout',() => this.interrupt = false)
+
     document.querySelectorAll('.testimonial-block__dots .dot').forEach((el,i) => el.addEventListener('click', () => this.dotClick(el,i)))
     window.addEventListener('resize', () => this.updateSlideWidth())
   }
@@ -66,6 +69,12 @@ class Carousel {
 
     if (this.currentSlide == 0) {
       this.pos = 0;
+    }
+    
+    if (this.currentSlide == this.length-1) {
+      carouselWrap.classList.add('remove_gradient');
+    } else {
+      carouselWrap.classList.remove('remove_gradient');
     }
 
     document.querySelector('.testimonial-block__carousel').scroll({
@@ -110,8 +119,24 @@ class Carousel {
   }
 }
 
-let slideWidth = document.querySelector('.testimonial-block__item').offsetWidth + parseInt(getComputedStyle(document.querySelector('.testimonial-block__item')).marginRight);;
-let carouselWrap = document.querySelector('.testimonial-block');
+let slideWidth = document.querySelector('.testimonial-block__item').offsetWidth + parseInt(getComputedStyle(document.querySelector('.testimonial-block__item')).marginRight);
+let carouselBlock = document.querySelector('.testimonial-block');
+let carouselWrap = document.querySelector('.testimonial-block__carousel-wrap');
 let carousel = document.querySelector('.testimonial-block__carousel');
 const testimonial = new Carousel(3000, carousel, slideWidth, carouselWrap);
 
+
+let mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+let mobileMenuHamburger = document.querySelector('.mobile-menu-hamburger');
+let mobileMenuClose = document.querySelector('.mobile-menu-overlay .feather-x');
+// Open Mobile Menu
+mobileMenuHamburger.addEventListener('click',function(){
+  document.documentElement.style.overflow = "hidden";
+  mobileMenuOverlay.classList.remove('mobile-menu-overlay--closed');
+});
+
+// Close Mobile Menu
+mobileMenuClose.addEventListener('click',function(){
+  document.documentElement.style.overflow = "auto";
+  mobileMenuOverlay.classList.add('mobile-menu-overlay--closed');
+});
