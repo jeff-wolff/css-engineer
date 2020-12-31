769869b3508a44c7891b6d42f4b1f7e482975fad
jeff <hi@jeffwolff.net>
2020-12-30 17:18:00 -0800

    remove favicon plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
@@ -44,7 +43,6 @@ module.exports = {
       },
       favicon: './src/images/logo.svg',
       template: './src/index.ejs'
-    }),
-    new FaviconsWebpackPlugin('./src/images/logo.svg')
+    })
   ]
 };
